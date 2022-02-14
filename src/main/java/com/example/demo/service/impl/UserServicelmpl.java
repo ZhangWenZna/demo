44a2d9d022dc102e07f6c015fb2e7389b1ea41f4
ZhangWenZna <84662944+ZhangWenZna@users.noreply.github.com>
2022-02-14 23:02:28 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/service/impl/UserServicelmpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public int getAgeByUsername(String username) {
+        return userMapper.getAgeByUsername(username);
+    }
+}
