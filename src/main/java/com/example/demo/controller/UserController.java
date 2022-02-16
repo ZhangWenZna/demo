ef07dfe87b6c91e210cb673c89d4e44c8bdaa199
ZhangWenZna <84662944+ZhangWenZna@users.noreply.github.com>
2022-02-16 20:49:28 +0800

    运行成功

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,15 +3,17 @@ package com.example.demo.controller;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cn")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @RequestMapping("/user/age")
+    @RequestMapping(value = "/user/age",  method = RequestMethod.GET )
     public int getAgeOfUser(){
         return userService.getAgeByUsername("springbootdemo");
     }
