8b0939e7a94a4e4577e661c74bd12797602ac47f
ZhangWenZna <84662944+ZhangWenZna@users.noreply.github.com>
2022-02-16 18:56:02 +0800

    first commit

--- src/main/java/com/example/demo/service/impl/UserServicelmpl.java
+++ src/main/java/com/example/demo/service/impl/UserServicelmpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-class UserServiceImpl implements UserService {
+public class UserServicelmpl implements UserService {
 
     @Autowired
     UserMapper userMapper;
