bedfea361275efbba1e6f409e910a7050c3b68b7
ZhangWenZna <84662944+ZhangWenZna@users.noreply.github.com>
2022-02-16 21:23:51 +0800

    修改错误信息

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//main
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
 public class DemoApplication {
